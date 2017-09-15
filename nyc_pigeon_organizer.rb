a96b653ca4690fcf67bbc3db1be3192667fe1740
laurkim <laurakeem@gmail.com>
2017-09-15 15:24:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |general_characteristic, hash|
+    hash.each do |specifics, birds|
+      birds.each do |bird|
+        if pigeon_list[bird].nil?
+          pigeon_list[bird] = {}
+        end
+        pigeon_list[bird][general_characteristic] ||= []
+        pigeon_list[bird][general_characteristic] << specifics.to_s
+      end
+    end
+  end
+  pigeon_list
 end
